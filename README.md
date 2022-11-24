99e2b5cfaea1c17b588106c3d527751048153d07
FlorenciaConci <112090605+FlorenciaConci@users.noreply.github.com>
2022-11-24 15:59:01 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Ludovica.repositorio
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-srpi3m)
---
+++ src/app/products.ts
@@ -0,0 +1,24 @@
+export const products = [
+  {
+    name: 'Choker mar blanco',
+    price: 800,
+    description: '35 cm de largo, ajustables y resistentes'
+  },
+  {
+    name: 'Pulsera ojo turco',
+    price: 350,
+    description: '30 cm de largo, ajustables, protectora contra el mal del ojo'
+  },
+  {
+    name: 'Tobilleras',
+    price: 450,
+    description: 'Hechas con hilo encerado de todos los colores y nudos disponibles'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
